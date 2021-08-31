50f73c6d16e42b56d6b5bf2eae93b5b15f9f9260
Karen Ascencio <ascencio.karen@yahoo.com.mx>
2021-08-31 16:30:43 -0500

    added template class component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React, { Component } from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super()
-    this.state = {
-      stage: ""
-    }
-    this.setStage = this.setStage.bind( this )
-  }
-
-  setStage( event ){
-    const value = event.target.value
-    console.log(value)
-    this.setState({ stage: value})
   }
 
   render(){
     return(
       <React.Fragment>
-        <input className="control" type="radio" name="stage" value="go" onClick={this.setStage}/>
-        <label for="" >Go</label>
-        <input className="control" type="radio" name="stage" value="caution" onClick={this.setStage}/>
-        <label for="" >Caution</label>
-        <input className="control" type="radio" name="stage" value="stop" onClick={this.setStage}/>
-        <label for="" >Stop</label>
-        <div className="traffic-light">
-          <div className={`light ${this.state.stage}`}></div>
-        </div>
       </React.Fragment>
     )
   }
