f9408d20c407c7d5c42d1bd9197421fdb0d54c97
Karen Ascencio <ascencio.karen@yahoo.com.mx>
2021-09-02 00:27:17 -0500

    added shop exercise by Plover

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
-import React, { Component } from 'react'
-import './App.css';
+import { description } from 'commander'
+import React, {useState} from 'react'
+import { Col, Row, Container, Card, Button, CardTitle, CardText, CardSubtitle, Jumbotron } from 'reactstrap'
+import storeData from "./mockData/index"
 
-class App extends Component {
-  constructor(){
-    super()
+console.log(storeData)
+
+const ShoppingItem = ({name, price, description, addingToCart, id}) => {
+  return(
+    <Col>
+      <Card body inverse color="info">
+        <CardTitle tag="h5">{ name }</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-muted"> ${ price }.00</CardSubtitle>
+        <CardText>{ description }</CardText>
+        <Button color="secondary" onClick={addingToCart} data-id={id}>Add to cart</Button>
+      </Card>
+    </Col>
+  )
+}
+
+const App = () => {
+  const { products } = storeData
+  const [cartProducts, setCartProducts] = useState([])
+
+  const addingToCart = event => {
+    const productId = event.target.dataset.id
+    setCartProducts([...cartProducts, products[productId]])
   }
 
-  render(){
-    return(
-      <React.Fragment>
-      </React.Fragment>
-    )
-  }
+  return(
+    <Container>
+      <Row>
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3"> $
+            {
+              cartProducts.reduce( (accum, item) => {
+                return accum += item.price
+              },0)
+            }.00
+          </h1>
+          <p className="lead">This is your total </p>
+          <hr className="my-2" />
+          <p className="lead">There's {cartProducts.length} products in your cart </p>
+        </Container>
+      </Jumbotron>
+      </Row>
+      <Row>
+        {
+          products.map( (item, index) => 
+          <ShoppingItem
+            {...item}
+            addingToCart= {addingToCart}
+            id= {index}
+          />)
+        }
+      </Row>
+    </Container>
+  )
+
 }
 
 export default App;
---
+++ src/mockData/index.js
@@ -0,0 +1,26 @@
+const data = {
+    products:[
+        {
+            name:"Producto 1",
+            price:50,
+            description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, ullam?"
+        },
+        {
+            name:"Producto 2",
+            price:80,
+            description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, ullam?"
+        },
+        {
+            name:"Producto 3",
+            price:10,
+            description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, ullam?"
+        },
+        {
+            name:"Producto 4",
+            price:55,
+            description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, ullam?"
+        }
+    ]
+}
+
+export default data
