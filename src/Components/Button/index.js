cfdf4b46b296557c122c9456dda8eced0741d805
Karen Ascencio <ascencio.karen@yahoo.com.mx>
2021-09-06 15:48:41 -0500

    added some colors

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function index() {
+export default function Button(props) {
+    const { percentage, clickHandler } = props
+
+
     return (
-        <div>
-            
-        </div>
+        <button data-percentage={percentage} className="text-white bg-darkCyan w-full p-1 rounded" onClick={clickHandler} >
+            {percentage}%
+        </button>
     )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      darkCyan: 'hsl(183, 100%, 15%)',
+      darkerGrayishCyan: 'hsl(186, 14%, 43%)',
+      darkGrayishCyan: 'hsl(184, 14%, 56%)',
+      lightGrayishCyan: 'hsl(185, 41%, 84%)',
+      lighterGrayishCyan: 'hsl(189, 41%, 97%)'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
